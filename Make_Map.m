1ee406d72b83d36cabbd08b22228a4bf83d6abb8
Brian Munsky <Munsky@users.noreply.github.com>
2020-03-19 08:05:13 -0600

    Changed color of infrction markers in map.
    Thanks to suggestion by Thanasis Boudalis.

--- Make_Map.m
+++ Make_Map.m
@@ -12,12 +12,22 @@ geoshow('landareas.shp', 'FaceColor', [0.15 0.5 0.15])
 I = floor(log2(app.DATA(:,j)));
 app.TimeSlider.Limits = [-Nt+1,0];
 
+%% change colormap
+cmap = colormap('jet');
+K = floor(linspace(1,size(cmap,1),floor(max(log2(app.DATA(:))))));
+
+
+%%
 for i=1:max(I)
-    geoshow(app.Lat(I==i),app.Long(I==i),'DisplayType', 'Point', 'Marker', 'o', 'Color', 'red','MarkerFaceColor', 'red','MarkerSize',i)
+    geoshow(app.Lat(I==i),app.Long(I==i),'DisplayType', 'Point', 'Marker', 'o', 'Color',cmap(K(i),:),'MarkerFaceColor',cmap(K(i),:),'MarkerSize',i)
 end
 
 copyobj(h.Children, app.map);  % Copy all of the axis' children to your app axis
 delete(h.Parent) % get rid of the figure created by worldmap()
+
+hcb = colorbar(app.map,'southoutside');
+set(get(hcb,'Xlabel'),'String','Size of infection')
+set(hcb,'Ticks',linspace(0,1,length(K(1:2:end))),'TickLabels',2.^[1:2:length(K)])
 
 
 
--- Make_Movie.m
+++ Make_Movie.m
@@ -23,10 +23,18 @@ for j = 1:Nt
     I = floor(log2(app.DATA(:,j)));
     app.TimeSlider.Limits = [-Nt+1,0];
     
+    cmap = colormap('hsv');
+    K = floor(linspace(1,size(cmap,1),max(log2(app.DATA(:)))));
     for i=1:max(I)
-        geoshow(app.Lat(I==i),app.Long(I==i),'DisplayType', 'Point', 'Marker', 'o', 'Color', 'red','MarkerFaceColor', 'red','MarkerSize',i)
+        geoshow(app.Lat(I==i),app.Long(I==i),'DisplayType', 'Point', 'Marker', 'o', 'Color',cmap(K(i),:),'MarkerFaceColor',cmap(K(i),:),'MarkerSize',i)
     end
+    
     title(app.dates{j})
+    
+    hcb = colorbar('southoutside');
+    set(get(hcb,'Xlabel'),'String','Size of infection')
+    set(hcb,'Ticks',linspace(0,1,length(K(1:2:end))),'TickLabels',2.^[1:2:length(K)])
+    
     drawnow
     currFrame = getframe(gcf);
     writeVideo(vidObj,currFrame);
