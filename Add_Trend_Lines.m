f70e6facee918bd38bc8f0dcab810e2b11ad3661
Brian Munsky <Munsky@users.noreply.github.com>
2020-03-19 15:20:28 -0600

    Update to show spread in individual states and countries.

--- Add_Trend_Lines.m
+++ Add_Trend_Lines.m
@@ -1,48 +1,88 @@
 function Add_Trend_Lines(app)
-N = length(app.inf_vs_t);
-n = app.days_for_trend.Value;
+Nt = size(app.inf_vs_t,2);
+Ns = size(app.inf_vs_t,1);
+nf = app.days_for_trend.Value;
 n0 = app.days_for_pred.Value;
-X = [1:N+14];
-x = [0:n]'; 
-
-%% Regresion for infections
-Y = log(app.inf_vs_t(N-n-n0:N-n0))';
-sxy = sum((x-mean(x)).*(Y-mean(Y))); sxx = sum((x-mean(x)).^2); syy = sum((Y-mean(Y)).^2);
-m = sxy/sxx; b = mean(Y)-m*mean(x);
-z1 = m*(X+n+n0-N)+b;
-Z1 = exp(z1);
-sr = sqrt((syy-m^2*sxx)/(n+2));
-sy = sr*sqrt(1+1/n+(X+n+n0-N-mean(x)).^2/sxx);
-UB = exp(z1+sy)-exp(z1);
-LB = -exp(z1-sy)+exp(z1);
+mn_x = find(max(app.inf_vs_t,[],1)>0,1,'first');
+X = [mn_x:Nt+5];
+xf = [0:nf]';
 
 hold(app.ax_infections,'off')
-errorbar(app.ax_infections,X,Z1,UB,LB,'b','linewidth',2)
-hold(app.ax_infections,'on')
-plot(app.ax_infections,[1:N],app.inf_vs_t,'ro',...
-    [N-n0+1:N],app.inf_vs_t(end-n0+1:end),'ko',...    
-    [1:N-n-n0],app.inf_vs_t(1:end-n-n0),'go',...
-    'MarkerSize',10,'MarkerFaceColor','auto','linewidth',3)
-legend(app.ax_infections,{'Model +/- std','fit data','validation data'},'Location','NorthWest')
-%% Regression for deaths
-Y = log(app.dth_vs_t(N-n-n0:N-n0))';
-sxy = sum((x-mean(x)).*(Y-mean(Y))); sxx = sum((x-mean(x)).^2); syy = sum((Y-mean(Y)).^2);
-m = sxy/sxx; b = mean(Y)-m*mean(x);
-z2 = m*(X+n+n0-N)+b;
-Z2 = exp(z2);
-sr = sqrt((syy-m^2*sxx)/(n+2));
-sy = sr*sqrt(1+1/n+(X+n+n0-N-mean(x)).^2/sxx);
-UB = exp(z2+sy)-exp(z2);
-LB = -exp(z2-sy)+exp(z2);
-
 hold(app.ax_deaths,'off')
-errorbar(app.ax_deaths,X,Z2,UB,LB,'b','linewidth',2)
-hold(app.ax_deaths,'on')
-plot(app.ax_deaths,[1:length(app.inf_vs_t)],app.dth_vs_t,'ro',...
-    [N-n0+1:N],app.dth_vs_t(end-n0+1:end),'ko',...    
-    [1:N-n-n0],app.dth_vs_t(1:end-n-n0),'go',...
-    'MarkerSize',10,'MarkerFaceColor','auto','linewidth',3)
-legend(app.ax_deaths,{'Model +/- std','fit data','validation data'},'Location','NorthWest')
 
+cmap = colormap('jet');
+K = floor(linspace(1,size(cmap,1),Ns));
+
+%% Regresion for infections
+for is = 1:Ns
+    Y = log(app.inf_vs_t(is,Nt-nf-n0:Nt-n0))';
+    x = xf(isfinite(Y));
+    Y = Y(isfinite(Y));
+    n=length(Y);
+    sxy = sum((x-mean(x)).*(Y-mean(Y))); sxx = sum((x-mean(x)).^2); syy = sum((Y-mean(Y)).^2);
+    m = sxy/sxx; b = mean(Y)-m*mean(x);
+    z1 = m*(X+nf+n0-Nt)+b;
+    Z1 = exp(z1);
+    sr = sqrt((syy-m^2*sxx)/(n+2));
+    sy = sr*sqrt(1+1/n+(X+nf+n0-Nt-mean(x)).^2/sxx);
+    UB = exp(z1+sy)-exp(z1);
+    LB = -exp(z1-sy)+exp(z1);
+    
+    if Ns==1
+        errorbar(app.ax_infections,X,Z1,UB,LB,'b','linewidth',2)
+        hold(app.ax_infections,'on')
+        plot(app.ax_infections,[1:Nt],app.inf_vs_t(is,:),'ro',...
+            [Nt-n0+1:Nt],app.inf_vs_t(is,end-n0+1:end),'ko',...
+            [1:Nt-n-n0],app.inf_vs_t(is,1:end-n-n0),'go',...
+            'MarkerSize',10,'MarkerFaceColor','auto','linewidth',3)
+        legend(app.ax_infections,{'Model +/- std','fit data','validation data'},'Location','NorthWest')
+    else
+        plot(app.ax_infections,X,Z1,'linewidth',2,'color',cmap(K(is),:))
+        hold(app.ax_infections,'on')
+        plot(app.ax_infections,[1:Nt],app.inf_vs_t(is,:),'o','color',cmap(K(is),:),...
+            'MarkerSize',10,'MarkerFaceColor','auto','linewidth',3)
+    end
+    
+    %% Regression for deaths
+    Y = log(app.dth_vs_t(is,Nt-nf-n0:Nt-n0))';
+    x = xf(isfinite(Y));
+    Y = Y(isfinite(Y));
+    n=length(Y);
+    sxy = sum((x-mean(x)).*(Y-mean(Y))); sxx = sum((x-mean(x)).^2); syy = sum((Y-mean(Y)).^2);
+    m = sxy/sxx; b = mean(Y)-m*mean(x);
+    z2 = m*(X+nf+n0-Nt)+b;
+    Z2 = exp(z2);
+    sr = sqrt((syy-m^2*sxx)/(n+2));
+    sy = sr*sqrt(1+1/n+(X+nf+n0-Nt-mean(x)).^2/sxx);
+    UB = exp(z2+sy)-exp(z2);
+    LB = -exp(z2-sy)+exp(z2);
+    
+    if Ns ==1
+        errorbar(app.ax_deaths,X,Z2,UB,LB,'b','linewidth',2)
+        hold(app.ax_deaths,'on')
+        plot(app.ax_deaths,[1:Nt],app.dth_vs_t(is,:),'ro',...
+            [Nt-n0+1:Nt],app.dth_vs_t(is,end-n0+1:end),'ko',...
+            [1:Nt-n-n0],app.dth_vs_t(is,1:end-n-n0),'go',...
+            'MarkerSize',10,'MarkerFaceColor','auto','linewidth',3)
+        legend(app.ax_deaths,{'Model +/- std','fit data','validation data'},'Location','NorthWest')
+    else
+        plot(app.ax_deaths,X,Z2,'color',cmap(K(is),:));
+        hold(app.ax_deaths,'on')
+        plot(app.ax_deaths,[1:Nt],app.dth_vs_t(is,:),'o','color',cmap(K(is),:),...
+            'MarkerSize',10,'MarkerFaceColor','auto','linewidth',3)
+    end
+    hold(app.ax_deaths,'on')
+end
+if Ns>1
+    if length(app.countries.Value)==1
+        TMP(1:2:2*length(app.states.Value)) = app.states.Value;
+        TMP(2:2:2*length(app.states.Value)) = app.states.Value;
+    else
+        TMP(1:2:2*length(app.countries.Value)) = app.countries.Value;
+        TMP(2:2:2*length(app.countries.Value)) = app.countries.Value;
+    end
+    legend(app.ax_deaths,TMP);
+    legend(app.ax_infections,TMP,'Location','eastoutside');
+end
 grid(app.ax_infections,'on')
 grid(app.ax_deaths,'on')
--- Make_Plot.m
+++ Make_Plot.m
@@ -3,12 +3,33 @@ if app.all.Value
     app.inf_vs_t = sum(app.DATA(:,10:end),1);
     app.dth_vs_t = sum(app.DATA_Deaths(:,10:end),1);
 elseif app.specific.Value
-    I = ismember(app.Countries,app.countries.Value);
-    app.inf_vs_t = sum(app.DATA(I,10:end),1);
-    app.dth_vs_t = sum(app.DATA_Deaths(I,10:end),1);
+    if strcmp(app.states.Value,'ALL')
+        for j=1:length(app.countries.Value)
+            I = ismember(app.Countries,app.countries.Value{j});
+            app.inf_vs_t(j,:) = sum(app.DATA(I,:),1);
+            app.dth_vs_t(j,:) = sum(app.DATA_Deaths(I,:),1);
+        end
+    else
+        I = ismember(app.Countries,app.countries.Value)&...
+            ismember(app.States,app.states.Value);
+        app.inf_vs_t = app.DATA(I,:);
+        app.dth_vs_t = app.DATA_Deaths(I,:);
+    end
 end
-plot(app.ax_infections,app.inf_vs_t,'rs')
-plot(app.ax_deaths,app.dth_vs_t,'rs')
+hold(app.ax_infections,'off')
+hold(app.ax_deaths,'off')
+legend(app.ax_deaths,'off')
+legend(app.ax_infections,'off')
+plot(app.ax_infections,[1:size(app.inf_vs_t,2)],app.inf_vs_t','-s')
+plot(app.ax_deaths,[1:size(app.inf_vs_t,2)],app.dth_vs_t','-s')
+if app.specific.Value
+    if ~strcmp(app.states.Value,'ALL')
+        app.states.Value
+        legend(app.ax_infections,app.states.Value,'Location','NorthWest')
+        legend(app.ax_deaths,app.states.Value,'Location','NorthWest')
+    end
+end
+
 
 if app.add_trend_lines.Value
     Add_Trend_Lines(app)
